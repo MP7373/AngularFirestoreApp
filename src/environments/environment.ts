bb7737dc0f553b128ba1a658cb4b5aac28a86171
MP7373 <martinpayne73@gmail.com>
2017-12-24 14:52:38 -0500

    initialize app and set up simple view of firestore data

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA4ELdvDSjtDKtdxspFgfwHy71wfu9je2Y",
+    authDomain: "epicangularprojectducksgoquack.firebaseapp.com",
+    databaseURL: "https://epicangularprojectducksgoquack.firebaseio.com",
+    projectId: "epicangularprojectducksgoquack",
+    storageBucket: "epicangularprojectducksgoquack.appspot.com",
+    messagingSenderId: "772477684439"
+  }
+};
